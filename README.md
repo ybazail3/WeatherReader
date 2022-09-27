61f03825e47256c4876838259dc0ac0170ff74bf
yazzy14 <yazminbazail@icloud.com>
2022-09-26 23:02:05 -0700

    Updated code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # WeatherReader
+
+## Getting Started
+    You are creating a weather app that list the daily weather and the 5-day forecast. You want your searches saved in local storage.
+
+## What it will look like
+<img src= '/Users/ybazail/Desktop/HomeWork/HW6-Weather/WeatherReader/images/Screen Shot 2022-09-26 at 12.22.26 AM.png'>
+
+## Deployment
+    -through github
+
+## Built With
+    * https://openweathermap.org/forecast5#list
+    * https://fonts.google.com/
+    * https://fontawesome.com/
+
+## Help
+    * https://www.w3schools.com/js/default.asp
+    * DevTools
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-
 var apiKey = '94a9647511f2a0ae40696b90c19eac3c';
-var city = 'seattle';
+var city;
 var todaysURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var searchBtn = document.querySelector('.submit-search');
 var search = document.querySelector('.search-bar');
@@ -8,132 +7,81 @@ var weatherLocation = document.querySelector('#weather-location');
 var iconn = document.querySelector('#icon');
 var tempp = document.querySelector('#temp');
 var humidityy = document.querySelector('#humidity');
-var windd = document.querySelector('#wind')
-
+var windd = document.querySelector('#wind');
+var todaysWeather = document.querySelector('.todays-weather');
+var dateDisplay = moment().format("MMM Do YY");
 var searchHistory = [];
 
-/*function showHistory() {
-    search.empty();
-    for (i=0; i < city.length; i++) {
-        var historyButton = document.createElement('button');
-        document.body.appendChild(historyButton)
-    }
-    JSON.parse(localStorage.getItem(search));
-}*/
+function saveCity(searchInput) {
+    searchHistory.push(searchInput)
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
+}
 
-function getWeather() {
+function showHistory() {
+    JSON.parse(localStorage.getItem('searchHistory', searchHistory));
+}
+
+function getWeather(city) {
     fetch(todaysURL + city + '&units=imperial&appid=' + apiKey)
         .then(function (response) {
             return response.json();
         })
-    /*if (!searchHistory.includes(data.name)) {
-        searchHistory.push(data.name)
-        localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
-        showHistory();
-    }*/
+        .then(function (data) {
+            //console.log(data);
+            document.querySelector('#date').innerHTML = dateDisplay;
+            weatherLocation.innerHTML = data.name;
+            iconn.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
+            tempp.innerHTML = 'Temp: ' + data.main.temp + "°F";
+            humidityy.innerHTML = 'Humidity: ' + data.main.humidity + "%";
+            windd.innerHTML = 'Wind: ' + data.wind.speed + " m/h";
 
-    .then(function (data) {
-        //console.log(data);
-        weatherLocation.innerHTML = data.name;
-        iconn.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
-        tempp.innerHTML = 'Temp: ' + data.main.temp + "°F";
-        humidityy.innerHTML = 'Humidity: ' + data.main.humidity + "%";
-        windd.innerHTML = 'Wind: ' + data.wind.speed + " m/h";
-    })
+            var lon = data.coord.lon;
+            var lat = data.coord.lat;
+            var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?&lat='
+            fetch(fiveDayURL + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    document.querySelector('#date-one').innerHTML = data.list[0].dt_txt;
+                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[0].weather[0].icon + '@2x.png';
+                    console.log(data)
+                    document.querySelector('#temp-one').innerHTML = 'Temp:' + data.list[0].main.temp + '°F';
+                    document.querySelector('#wind-one').innerHTML = 'Wind: ' + data.list[0].wind.speed + ' m/h';
+                    document.querySelector('#humidity-one').innerHTML = 'Humidity: ' + data.list[0].main.humidity + '%';
+
+                    document.querySelector('#date-two').innerHTML = data.list[8].dt_txt;
+                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[1].weather[1].icon + '@2x.png';
+                    document.querySelector('#temp-two').innerHTML = 'Temp: ' + data.list[8].main.temp + '°F';
+                    document.querySelector('#wind-two').innerHTML = 'Wind: ' + data.list[8].wind.speed + ' m/h';
+                    document.querySelector('#humidity-two').innerHTML = 'Humidity: ' + data.list[8].main.humidity + '%';
+
+                    document.querySelector('#date-three').innerHTML = data.list[16].dt_txt;
+                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[2].weather[2].icon + '@2x.png';
+                    document.querySelector('#temp-three').innerHTML = 'Temp: ' + data.list[16].main.temp + '°F';
+                    document.querySelector('#wind-three').innerHTML = 'Wind: ' + data.list[16].wind.speed + ' m/h';
+                    document.querySelector('#humidity-three').innerHTML = 'Humidity: ' + data.list[16].main.humidity + '%';
+
+                    document.querySelector('#date-four').innerHTML = data.list[24].dt_txt;
+                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[3].weather[3].icon + '@2x.png';
+                    document.querySelector('#temp-four').innerHTML = 'Temp: ' + data.list[24].main.temp + '°F';
+                    document.querySelector('#wind-four').innerHTML = 'Wind: ' + data.list[24].wind.speed + ' m/h';
+                    document.querySelector('#humidity-four').innerHTML = 'Humidity: ' + data.list[24].main.humidity + '%';
+
+                    document.querySelector('#date-five').innerHTML = data.list[32].dt_txt;
+                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[4].weather[4].icon + '@2x.png';
+                    document.querySelector('#temp-five').innerHTML = 'Temp: ' + data.list[32].main.temp + '°F';
+                    document.querySelector('#wind-five').innerHTML = 'Wind: ' + data.list[32].wind.speed + ' m/h';
+                    document.querySelector('#humidity-five').innerHTML = 'Humidity: ' + data.list[32].main.humidity + '%';
+                })
+        })
 }
-getWeather();
 
 searchBtn.addEventListener('click', function (event) {
     event.preventDefault();
     city = search.value;
-    getWeather();
-    getForecast();
+    getWeather(city);
+    saveCity();
 })
-//trying to get the days
-//document.querySelector('#date').text(data.name + '(' + moment(data.dt, 'X').format('L') + ')');
-
-var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?q='
-function getForecast() {
-    fetch(fiveDayURL + city + '&units=imperial&appid=' + apiKey)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            document.querySelector('#date-one').innerHTML = data.dt;
-            iconn.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
-            document.querySelector('#temp-one').innerHTML = 'Temp:' + data.main.temp + '°F';
-            document.querySelector('#wind-one').innerHtml = 'Wind: ' + data.wind.speed + ' m/h';
-            document.querySelector('#humidity-one').innerHTML = 'Humidity: ' + data.main.humidity + '%';
-        })
-        .then(function (data) {
-            document.querySelector('#date-two').innerHTML = data.dt;
-            iconn.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
-            document.querySelector('#temp-two').innerHTML = 'Temp: ' + data.main.temp + '°F';
-            document.querySelector('#wind-two').innerHtml = 'Wind: ' + data.wind.speed + ' m/h';
-            document.querySelector('#humidity-two').innerHTML = 'Humidity: ' + data.main.humidity + '%';
-        })
-        .then(function (data) {
-            document.querySelector('#date-three').innerHTML = data.dt;
-            iconn.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
-            document.querySelector('#temp-three').innerHTML = 'Temp: ' + data.main.temp + '°F';
-            document.querySelector('#wind-three').innerHtml = 'Wind: ' + data.wind.speed + ' m/h';
-            document.querySelector('#humidity-three').innerHTML = 'Humidity: ' + data.main.humidity + '%';
-        })
-        .then(function (data) {
-            document.querySelector('#date-four').innerHTML = data.dt;
-            iconn.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
-            document.querySelector('#temp-four').innerHTML = 'Temp: ' + data.main.temp + '°F';
-            document.querySelector('#wind-four').innerHtml = 'Wind: ' + data.wind.speed + ' m/h';
-            document.querySelector('#humidity-four').innerHTML = 'Humidity: ' + data.main.humidity + '%';
-        })
-        .then(function (data) {
-            document.querySelector('#date-five').innerHTML = data.dt;
-            iconn.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
-            document.querySelector('#temp-five').innerHTML = 'Temp: ' + data.main.temp + '°F';
-            document.querySelector('#wind-five').innerHtml = 'Wind: ' + data.wind.speed + ' m/h';
-            document.querySelector('#humidity-five').innerHTML = 'Humidity: ' + data.main.humidity + '%';
-        })
-}
-getForecast();
-
-
-
-
-
-/*var dayForecast = {
-    apiKey: '94a9647511f2a0ae40696b90c19eac3c',
-    getWeather: function (city) {
-        fetch(queryURL)
-        .then((response) => {
-            if (!response.ok) {
-              alert("No weather found.");
-              throw new Error("No weather found.");
-            }
-            return response.json();
-        })
-            .then((data) => this.displayWeather(data))
-    },
-    displayForecast: function (data) {
-        var { name } = data;
-        var { icon } = data.weather[0];
-        var { temp } = data.main;
-        var { speed } = data.wind;
-        var { humidity } = data.pressure;
-        console.log(name, icon, description, temp, humidity, speed, uvi);
-        document.querySelector('#weather-location').innerText = name;
-        document.querySelector('#icon').src =
-            "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector('#temp').innerText = temp + "°F";
-        document.querySelector('#humidity').innerText =
-            humidity + "%";
-        document.querySelector('#wind').innerText =
-            speed + " m/h";
-    },
-    search: function () {
-        this.getWeather(document.querySelector('.search-bar').value);
-    },
-};
-document.querySelector('.submit-search').addEventListener("click", function () {
-    weather.search();
-});*/
+showHistory()
